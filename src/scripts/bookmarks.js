f989cf2ad52dc9c0e962d959cf6fb49e5f527c89
Cesar Morales <cesarlenin2@live.com>
2020-03-27 19:45:27 -0700

    flip the star radio

--- src/scripts/bookmarks.js
+++ src/scripts/bookmarks.js
@@ -28,16 +28,16 @@ const generateBookmarkControls = function() {
             <label for="description">Description:</label>
             <input type="text" id="description" name="desc" placeholder="longer description goes here">
             <section class="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
             <input type="radio" id="star1" name="rate" value="1" />
             <label for="star1" title="text">1 star</label>
+            <input type="radio" id="star2" name="rate" value="2" />
+            <label for="star2" title="text">2 stars</label>
+            <input type="radio" id="star3" name="rate" value="3" />
+            <label for="star3" title="text">3 stars</label>
+            <input type="radio" id="star4" name="rate" value="4" />
+            <label for="star4" title="text">4 stars</label>
+            <input type="radio" id="star5" name="rate" value="5" />
+            <label for="star5" title="text">5 stars</label>
             </section>
             <button type= "submit" class="submit-button button">Submit</button>
         </form>
